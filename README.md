dfb25b4927a92f3d8c0a563bf9242a52e455517e
Alexander <github@lichter.io>
2023-11-24 15:13:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reduce the size of your payload and `__NUXT__` - Nuxt Performance in Depth
+
+> **Note**
+> This repository contains the code for the YouTube Video [Reduce the size of your payload and `__NUXT__` - Nuxt Performance in Depth](https://www.youtube.com/watch?v=laRJNkG_wls).
+
+We explore the `payload`, also sometimes referred to as `__NUXT__` and how to reduce it's size in your app!
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/endpoint', {
+  transform(input) {
+    return {
+      name: input.item.name,
+      flavorText: input.item.flavorText,
+      iconSrc: input.item.iconSrc
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ data?.name }}</h1>
+    <h2>{{ data?.flavorText }}</h2>
+    <img :src="data?.iconSrc">
+  </div>
+</template>
